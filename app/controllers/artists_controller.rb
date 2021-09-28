97e64ac98654dcbd7c44c7d70b9f86144e52bb0d
Carlj0666 <carlj1881@yahoo.com>
2021-09-27 17:47:59 -0700

    Updated show

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -6,7 +6,7 @@ class ArtistsController < ApplicationController
 
     def show
         artist = Artist.find(params[:id])
-        render json: ArtSerializer.new(artist)
+        render json: ArtistSerializer.new(artist)
     end
 
 
